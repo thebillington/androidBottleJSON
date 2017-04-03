88a34ffd2cb7e8ee42fae4fd91995057dbed3d17
Billy Rebecchi <b.rebecchi@newcastle.ac.uk>
2017-04-03 23:00:57 +0100

    Add a POST method for the HTTP client

--- JSONExample/app/src/main/java/uk/co/thebillington/jsonexample/HTTPClient.java
+++ JSONExample/app/src/main/java/uk/co/thebillington/jsonexample/HTTPClient.java
@@ -1,9 +1,15 @@
 package uk.co.thebillington.jsonexample;
 
+import android.content.Context;
+import android.util.Log;
+
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
@@ -13,6 +19,8 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
+
+import cz.msebera.android.httpclient.entity.StringEntity;
 
 /**
  * Created by thebillington on 08/03/17.
@@ -27,6 +35,23 @@ public class HTTPClient {
         client.addHeader("Accept", "application/json");
         client.addHeader("Content-Type", "application/json");
         client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public static void post(Context context, String url, JSONObject params, AsyncHttpResponseHandler responseHandler) {
+
+        StringEntity entity = null;
+
+        try {
+            //Create an entity to send as parameters
+            entity = new StringEntity(params.toString());
+            //Set header to JSON
+            client.addHeader("Accept", "application/json");
+            client.addHeader("Content-Type", "application/json");
+            //Post and receive response
+            client.post(context, getAbsoluteUrl(url), entity, "application/json", responseHandler);
+        } catch (UnsupportedEncodingException e) {
+            Log.d("UEE", "Cannot process request: " + e);
+        }
     }
 
     private static String getAbsoluteUrl(String relativeUrl) {
