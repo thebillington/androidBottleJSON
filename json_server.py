4d585510a107655bab607082ee67a924b3d7b671
Billy Rebecchi <b.rebecchi@newcastle.ac.uk>
2017-04-03 22:58:10 +0100

    Add basic login functionality

--- json_server.py
+++ json_server.py
@@ -1,6 +1,14 @@
 import os
-from bottle import route, run, template, response
+from bottle import route, get, post, run, response, request
 from json import dumps
+import json
+from urllib2 import urlopen
+
+#Create the index
+@route('/')
+def index():
+	response.content_type = "application/json"
+	return dumps({"message":"Welcome to test server"})
 
 #Create our json endpoint
 @route('/json')
@@ -8,6 +16,39 @@ def json():
 	rv = {"title":"JSON test", "description":"Bottle server to test JSON response"}
 	response.content_type = "application/json"
 	return dumps(rv)
+
+#Create a get request for logging in
+@get('/login')
+def login():
+	return '''
+        <form action="/login" method="post">
+            Username: <input name="username" type="text" />
+            Password: <input name="password" type="password" />
+            <input value="Login" type="submit" />
+        </form>
+    	'''
+
+#Create our post function for logging in
+@post('/login')
+def do_login():
+	username = request.forms.get('username')
+	password = request.forms.get('password')	
+	print("Attempted login from account {} with password {}".format(username, password))
+	rv = {"message":"successful login from {}".format(username)}
+	response.content_type = "application/json"
+	return dumps(rv)
+
+
+#Create our post function for logging in
+@post('/mlogin')
+def do_mlogin():
+	request.content_type = "application/json"
+        username = request.json['username']
+        password = request.json['password']
+        print("Attempted login from account {} with password {}".format(username, password))
+        rv = {"message":"successful login from {}".format(username)}
+        response.content_type = "application/json"
+        return dumps(rv)
 
 #Run the app if not an import
 if __name__ == "__main__":
